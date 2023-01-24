16f66444eb9caf0c3dd4874501aa100600962241
mjahanmktx <123376809+mjahanmktx@users.noreply.github.com>
2023-01-24 08:33:03 -0500

    2 files modified

--- Post.tsx
+++ Post.tsx
@@ -5,17 +5,23 @@ import Comment from './Comment';
 import Author from './Author';
 
 export default function Post(params) {
+  const [body, setBody] = React.useState(params.post.body);
   return (
     <div>
       <h1>{params.post.title}</h1>
       {params.post.allAuthors.map((allAuthors) => (
         <Author name={allAuthors} />
       ))}
-      <p>{params.post.body}</p>
+      <p>{body}</p>
       <h3>Comments:</h3>
       {params.post.comments.map((comment) => (
         <Comment body={comment} />
       ))}
+      <input
+        type="text"
+        placeholder="Update post body here"
+        onChange={(textboxInput) => setBody(textboxInput.target.value)}
+      ></input>
     </div>
   );
 }
